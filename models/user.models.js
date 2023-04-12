3e760473c3df2564ed2550c9207ba852ef5ff52b
Tiliksew <tiliksewmulugeta552@gmail.com>
2023-04-12 20:49:33 +0300

    new model fields added

--- models/user.models.js
+++ models/user.models.js
@@ -2,19 +2,38 @@
  
 
 const mongoose = require('mongoose');
-const { getDecimal } = require('../utils/db');
+const { getDecimal,isDecimal,isInteger } = require('../utils/db');
 const { Schema} = mongoose
 const UserSchema = new mongoose.Schema(
-    {
-        id: { type: Schema.Types.Number, default: null },
-        // name: { type: String, default: null },
-        lt: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
-        la: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
-        speed: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
-        acceleration: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
-        isActive: { type: Boolean, default: true},
+  {
+    id: { type: Schema.Types.Number, validate: [isInteger, "Invalid Value"],default: null },
+    lt: {
+      type: Schema.Types.Decimal128,
+      validate: [isDecimal, "Value must be decimal"],
+      get: getDecimal,
+      default: 0,
     },
-    { timestamps: true,toJSON: { getters: true }  },
+    la: {
+      type: Schema.Types.Decimal128,
+      validate: [isDecimal, "Value must be decimal"],
+      get: getDecimal,
+      default: 0,
+    },
+    speed: {
+      type: Schema.Types.Decimal128,
+      validate: [isDecimal, "Value must be decimal"],
+      get: getDecimal,
+      default: 0,
+    },
+    acceleration: {
+      type: Schema.Types.Decimal128,
+      validate: [isDecimal, "Value must be decimal"],
+      get: getDecimal,
+      default: 0,
+    },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true, toJSON: { getters: true } }
 );
 
 module.exports = mongoose.model('User', UserSchema);
--- utils/db.js
+++ utils/db.js
@@ -4,3 +4,37 @@ exports. getDecimal = (value) =>{
     }
     return value;
 }
+exports.isDecimal = (x) => {
+
+    // check if the passed value is a number
+    if(typeof x == 'number' && !isNaN(x)){
+    
+        // check if it is integer
+        if (Number.isInteger(x)) {
+            false
+        }
+        else {
+            true
+        }
+    
+    } else {
+        true
+    }
+}
+exports.isInteger = (x) => {
+
+    // check if the passed value is a number
+    if(typeof x == 'number' && !isNaN(x)){
+    
+        // check if it is integer
+        if (Number.isInteger(x)) {
+            true
+        }
+        else {
+            false
+        }
+    
+    } else {
+        true
+    }
+}
