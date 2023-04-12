56e5320001e01f0946c8f7fc046964b14cd0579f
Tiliksew <tiliksewmulugeta552@gmail.com>
2023-04-12 20:32:05 +0300

    new model fields added

--- models/user.models.js
+++ models/user.models.js
@@ -2,19 +2,19 @@
  
 
 const mongoose = require('mongoose');
-const dbHelpers= require('../utils/db');
+const { getDecimal } = require('../utils/db');
 const { Schema} = mongoose
 const UserSchema = new mongoose.Schema(
     {
         id: { type: Schema.Types.Number, default: null },
         // name: { type: String, default: null },
-        lt: { type: Schema.Types.Decimal128, get: dbHelpers.getDecimal,default: 0 },
-        la: { type: Schema.Types.Decimal128,get: dbHelpers.getDecimal, default: 0 },
-        speed: { type: Schema.Types.Decimal128,get:dbHelpers.getDecimal, default: 0 },
-        acceleration: { type: Schema.Types.Decimal128,get:dbHelpers.getDecimal, default: 0 },
+        lt: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
+        la: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
+        speed: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
+        acceleration: { type: Schema.Types.Decimal128,get:getDecimal, default: 0 },
         isActive: { type: Boolean, default: true},
     },
-    { timestamps: true,toJSON: { getters: true } },
+    { timestamps: true,toJSON: { getters: true }  },
 );
 
 module.exports = mongoose.model('User', UserSchema);
